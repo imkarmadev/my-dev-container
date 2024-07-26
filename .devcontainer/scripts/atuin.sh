2df437dd5773d9e446d8a9058c0e014f22e1060d
Andrew Pozyniuk <imkarma.dev@gmail.com>
2024-07-26 21:18:30 +0000

    replace hard langs install to devcontainer features

--- .devcontainer/scripts/atuin.sh
+++ .devcontainer/scripts/atuin.sh
@@ -9,6 +9,12 @@ fi
 
 source "$ENV_DIRECTORY"
 
+curl https://setup.atuin.sh -sSf | bash -s -- -y
+
+echo 'eval "$(atuin init zsh)"' >> $HOME/.zshrc
+
+source $HOME/.zshrc
+
 echo "Login to Atuin history as $ATUIN_LOGIN ..."
 
 atuin login -u "$ATUIN_LOGIN" -p "$ATUIN_PASSWORD" -k "$ATUIN_KEY"
